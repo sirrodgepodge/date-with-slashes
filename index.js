a22abf4ea671db5b972e143b1addfd04497e3908
Roger Beaman <rwbeaman@gmail.com>
2016-06-10 23:42:23 -0400

    author fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-module.exports = function prettyDate(input, fourDigitYear) {
+module.exports = function dateWithSlashes(input, fourDigitYear) {
   var tempDate = new Date(input);
   return tempDate.getMonth() + 1 + "/" + tempDate.getDate() + "/" + tempDate.getFullYear().toString().slice(fourDigitYear? 4 : 2);
 }
